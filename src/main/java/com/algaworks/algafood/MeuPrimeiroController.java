158fca10f3bf74d743e4e1b80d2253b44076c1ea
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-03-25 22:50:54 -0300

    Vídeo #F016 ESR-2.06

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,4 +1,19 @@
 package com.algaworks.algafood;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+//Criamos um método simples abaixo
+@Controller
 public class MeuPrimeiroController {
+    @GetMapping("/hello")
+    //ou seja, quando houver a chamada na URL de localhost:8080/hello, ele
+    //irá devolver a frase do return(Resposta)
+    @ResponseBody
+    public String hello() {
+        return "Hello Guilherme, você rodou seu segundo projeto " +
+                "Spring Boot";
+    }
 }
